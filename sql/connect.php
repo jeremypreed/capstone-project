0e7c9e12abd344bd0a6a8b21564c2c5b173adfa7
Jeremy <jeremy.p.reed@gmail.com>
2016-08-24 04:57:05 -0500

    Delete connect.php
    moved contents to config.php

--- sql/connect.php
+++
@@ -1,5 +0,0 @@
-<?php
-$dbconnect = mysqli_connect($_['DB_HOST'],$_['DB_USER'],$_['DB_PASS'],$_['DB_NAME']); // Conects to the database
-if (mysqli_connect_errno()) { // If error connecting
-	echo '<div id="error">Failed to connect: '.mysqli_connect_errno().'</div>'; // Display error message
-}
