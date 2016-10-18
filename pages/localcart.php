2f424c33ad63066c084b1d73a3a6da3cb79af328
Jeremy <jeremy.p.reed@gmail.com>
2016-10-17 21:42:49 -0500

    Changed ng-show/ng-hide to ngif statements.

--- pages/localcart.php
+++ pages/localcart.php
@@ -1,15 +1,15 @@
 <div ng-controller="CartController as cart">
-	<div ng-hide="cart.products[0]">
+	<div ng-if="cart.products.length===0">
 		<p>Your cart is empty.</p>
 	</div>
 
-	<div class="row cart no-padding" ng-show="cart.products[0]">
+	<div class="row cart no-padding" ng-if="cart.products.length>0">
 		<div class="col-md-8"></div>
 		<div class="col-md-2 cart-head text-center">Quantity</div>
 		<div class="col-md-2 cart-head text-right">Price</div>
 	</div>
 	
-	<div class="row cart" ng-repeat="product in cart.products track by $index" ng-class="cart.alternate($index)">
+	<div class="row cart alt" ng-repeat="product in cart.products track by $index">
 		<div class="col-md-2 col-xs-4 cart-img text-center">
 			<a ng-href="{{ '<?php $_['SITE_URL']; ?>shop/' + product.category + '/' + product.subcategory + '/' + product.id }}">
 				<img ng-src="{{ '<?php $_['SITE_URL']; ?>' + product.image}}">
@@ -21,10 +21,10 @@
 		</div>
 		<div class="col-md-2 col-xs-4 cart-items text-left">
 				<button ng-click="quantity=quantity-1" ng-disabled="quantity==1" class="update">&minus;</button>
-				<input type="number" ng-model="quantity" ng-init="quantity=product.quantity" class="update"/>
+				<input type="number" ng-model="quantity" ng-init="quantity=product.quantity" class="update" min="1" max="99" />
 				<button ng-click="quantity=quantity+1" ng-disabled="quantity==99" class="update">&plus;</button>
 				
-				<button type="submit" class="update" ng-click="cart.updateProduct($index,quantity);" ng-hide="product.quantity==quantity||quantity<1">Update</button><br>
+				<button type="submit" class="update" ng-click="cart.updateProduct($index,quantity);" ng-if="product.quantity!==quantity">Update</button><br>
 				
 				<button type="submit" class="btn btn-link remove" ng-click="cart.removeProduct($index);">
 					<i class="fa fa-remove fw"></i> Remove
@@ -32,27 +32,27 @@
 			<form method="post" action="#"></form>
 		</div>
 		<div class="col-md-2 col-xs-8 cart-price text-right">
-			<span class="small" ng-hide="product.quantity==1">
+			<span class="small" ng-if="product.quantity>1">
 				{{ product.discount_price | currency }} &times; {{ product.quantity }}<br>
 			</span>
-			<span class="price" ng-show="product.price==product.discount_price">
+			<span class="price" ng-if="product.price===product.discount_price">
 				{{ product.price * product.quantity | currency }}
 			</span>
-			<span class="discount" ng-hide="product.price==product.discount_price">
+			<span class="discount" ng-if="product.price!=product.discount_price">
 				{{ product.price * product.quantity | currency }}
 			</span>
-			<span class="price-red" ng-hide="product.price==product.discount_price">
+			<span class="price-red" ng-if="product.price!=product.discount_price">
 				{{ product.discount_price * product.quantity | currency }}
 			</span>
-			<span class="small" ng-hide="product.amount_off==0">
+			<span class="small" ng-if="product.amount_off!=0">
 				<br>{{ product.percent_off }} Off
 				<br>You save {{ product.amount_off * product.quantity | currency }}
 			</span>
 		</div>
 	</div>
 	
-	<div ng-show="cart.products[0]">
-		<div class="row cart no-padding">
+	<div ng-if="cart.products.length>0">
+		<div class="row cart">
 			<div class="col-md-8"></div>
 			<div class="col-md-4 cart-head text-right">Subtotal</div>
 		</div>
@@ -62,7 +62,7 @@
 			<div class="col-md-5 subtotal">
 					{{ cart.subtotal | currency }}<br>
 				<span class="small">
-					<span ng-hide="cart.savings==0">
+					<span ng-if="cart.savings!=0">
 						You save {{ cart.savings | currency }} on 
 					</span>
 					{{ cart.quantity }} item(s)
